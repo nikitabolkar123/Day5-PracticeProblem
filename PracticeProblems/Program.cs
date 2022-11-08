aeb6b6576dbd01d4bcf8ba7f97d3c20b3315e11f
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-08 03:54:34 -0800

    Added

---
+++ PracticeProblems/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a no");
+            int n = int.Parse(Console.ReadLine());
+            EvenOdd(n);
+        }
+        public static void EvenOdd(int no)
+        {
+
+            if (no % 2 == 0)
+            {
+                Console.WriteLine("no is even");
+            }
+            else
+            {
+                Console.WriteLine("no is odd");
+            }
+        }
+    }
+}
